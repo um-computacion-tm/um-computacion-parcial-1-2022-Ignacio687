de087c9ca42c48bb7c4d1b629d683a2a3c6bff88
Ignacio687 <ignadavidchaves@gmail.com>
2022-06-14 08:43:24 -0300

    firstTestPass

--- hangman.py
+++ hangman.py
@@ -1,3 +1,16 @@
 
 class Hangman():
+    def __init__(self):
+        self.word = []
+        self.lifes = 5
+        self.displeyWord = []
+
+    def set_word(self, word):
+        self.word = [letter for letter in word]
+        self.displeyWord = ['_ ' for letter in self.word]
+
+    def show(self):
+        displeyWordString = ''.join(self.displeyWord)
+        return f'Lifes: {self.lifes} - Word: {displeyWordString}'
+
     
